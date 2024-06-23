cad177eeac53a98c9e8a4cbeff13dac36f532648
Alexander Kung <alexanderkung@gmail.com>
2024-06-23 02:15:48 -0700

    api change

--- api.py
+++ api.py
@@ -7,45 +7,29 @@ def store_dataframe(df : pd.DataFrame):
     chroma_client = chromadb.PersistentClient(path=PERSISTENT_STORAGE)
     collection = chroma_client.get_or_create_collection(name=COLLECTION_NAME, embedding_function=EMBEDDING_FUNCTION)
     
-    if not os.path.exists(f"{PERSISTENT_STORAGE}/base_id.txt"):
-        with open(f"{PERSISTENT_STORAGE}/base_id.txt", 'w') as f:
-            f.write("0")
-
-    with open(f"{PERSISTENT_STORAGE}/base_id.txt", 'r') as f:
-        BASE_ID = int(f.read())
-
     texts = df['text'].tolist()
-    ids = [str(i) for i in range(BASE_ID, BASE_ID + len(texts))]
-    BASE_ID += len(texts)
-    metadatas = df.drop(columns=['text']).to_dict(orient='records') 
+    ids = [str(i) for i in range(0, len(texts))]
+    df.drop(columns=['text'], inplace=True)
+    metadatas = df.to_dict(orient='records')
     
-    collection.add(
-        documents=texts,
-        ids=ids,
-        metadatas=metadatas
-    )
+    # collection.add(
+    #     documents=texts,
+    #     ids=ids,
+    #     metadatas=metadatas
+    # )
 
-    with open(f"{PERSISTENT_STORAGE}/base_id.txt", 'w') as f:
-        f.write(str(BASE_ID))
+    metadata_column_types = dict(df.dtypes)
+    metadata_type_and_classes = {}
+    for column_name, column_type in metadata_column_types.items():
+        if column_type == 'object':
+            print(str(column_type))
+            uniques_values = df[column_name].unique()
+            if len(uniques_values) < 30:
+                metadata_type_and_classes[column_name] = (str(column_type), list(uniques_values))
+        else:
+            metadata_type_and_classes[column_name] = (str(column_type), None)
 
-    # for index, row in df.iterrows():
-    #     print(index)
-    #     row_dict = row.to_dict()
-    #     text = row_dict['text']
-    #     cur_id = str(index)
-        
-    #     del row_dict['text']
-    #     metadata = row_dict
-
-    #     # print(text)
-    #     # print(metadata)
-
-    #     collection.add(
-    #         documents=[text],
-    #         ids=[cur_id],
-    #         metadatas=[metadata]
-    #     )
-
+    return metadata_type_and_classes
 
 def query(text : str):
     chroma_client = chromadb.PersistentClient(path=PERSISTENT_STORAGE)
@@ -53,15 +37,17 @@ def query(text : str):
     
     query_result = collection.query(
         query_texts=[text],
-        n_results=1
+        n_results=5
     )
     return query_result
 
 
 
-from pprint import pprint
-pprint(query("I love anime"))
 
-# df = pd.read_csv('./dataset/formatted-reddit-dataset/entertainment_anime.csv')
 
-# store_dataframe(df)
+# from pprint import pprint
+# pprint(query("I love anime"))
+
+df = pd.read_csv('./dataset/formatted-reddit-dataset/entertainment_anime.csv')
+
+store_dataframe(df)
--- constant.py
+++ constant.py
@@ -3,7 +3,7 @@ import torch
 
 COLLECTION_NAME = "collection"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
-PERSISTENT_STORAGE = "_vector_db"
+PERSISTENT_STORAGE = "vector_db"
 
 device = "cpu"
 
